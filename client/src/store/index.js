80574fe3bb16b0dd9511fbf5f4be2c1044bef9e5
faizal <71105723+sfaizal786@users.noreply.github.com>
2025-07-01 15:06:42 -0700

    Delete client directory

--- client/src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { create} from "zustand";
-import { createAuthslice} from "./slices/auth-slice";
-import { createChatSlice } from "./slices/chat-slice";
-
-export const useAppStore = create()((...a)=>({
-    ...createAuthslice(...a),
-    ...createChatSlice(...a),
-}))
