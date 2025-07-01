3d8e7f7f7393442e3e6dd6e90b4374c1c42873a2
faizal <71105723+sfaizal786@users.noreply.github.com>
2025-07-01 16:27:32 -0700

    Update Authroutes.js

--- server/routes/Authroutes.js
+++ server/routes/Authroutes.js
@@ -1,21 +1,30 @@
 import { Router } from "express";
-import { getUserInfo, login, signup ,updateProfile, addProfileImage,removeProfileImage, logout, } from "../controllers/AuthController.js";
+import {
+  getUserInfo,
+  login,
+  signup,
+  updateProfile,
+  addProfileImage,
+  removeProfileImage,
+  logout,
+} from "../controllers/AuthController.js";
 import { verifyToken } from "../middleware/Authmiddleware.js";
 import multer from "multer";
 
-
 const authroutes = Router();
-const upload = multer({dest:"uploads/profiles/"});
+const upload = multer({ dest: "uploads/profiles/" });
+
 authroutes.post("/signup", signup);
 authroutes.post("/login", login);
-authroutes.post("/updateprofile", verifyToken,updateProfile);
+authroutes.post("/update-profile", verifyToken, updateProfile);
 authroutes.get("/userinfo", verifyToken, getUserInfo);
 authroutes.post(
-    "/addprofileimage",
-    verifyToken , 
-    upload.single("profile-image"),
-    addProfileImage
+  "/add-profile-image",
+  verifyToken,
+  upload.single("profile-image"),
+  addProfileImage
 );
 authroutes.delete("/remove-profile-image", verifyToken, removeProfileImage);
 authroutes.post("/logout", logout);
+
 export default authroutes;
