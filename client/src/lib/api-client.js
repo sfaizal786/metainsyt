f628f5d03de6b365dfdfdf2ebebc81cb5c1028e6
faizal <71105723+sfaizal786@users.noreply.github.com>
2025-07-02 09:29:57 +0530

    Update api-client.js

--- client/src/lib/api-client.js
+++ client/src/lib/api-client.js
@@ -1,8 +1,8 @@
-import { HOST } from "@/utils/constant.js";
+// src/lib/api-client.js
 import axios from "axios";
+import { HOST } from "@/utils/constant.js";
 
-
-export const apiClient = axios.create({ 
-    baseURL: HOST,
-
+export const apiClient = axios.create({
+  baseURL: HOST,
+  withCredentials: true, // ✅ Ensures cookies (JWT) are always sent
 });
