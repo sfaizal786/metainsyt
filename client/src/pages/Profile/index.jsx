dcabaacfa7c43239be30f0f028c884d77a62751c
faizal <71105723+sfaizal786@users.noreply.github.com>
2025-07-03 08:55:29 -0700

    Update profile index.jsx

--- client/src/pages/Profile/index.jsx
+++ client/src/pages/Profile/index.jsx
@@ -138,4 +138,106 @@ function Profile() {
     <div className='bg-[#1b1c24] h-[100vh] flex items-center justify-center flex-col gap-10'>
       <div className='flex flex-col gap-10 w-[80vw] md:w-max'>
         <div onClick={handleNavigate}>
-          <IoArrowBa
+          <IoArrowBack className='text-4xl lg:text-6xl text-white/90 cursor-pointer' />
+        </div>
+        <div className='grid grid-cols-2'>
+          <div
+            className='h-full w-32  md:w-48 md:h-48 relative flex items-center justify-center'
+            onMouseEnter={() => sethovered(true)}
+            onMouseLeave={() => sethovered(false)}
+          >
+            <Avatar className='h-32 w-32 md:w-48 md:h-48 rounded-full overflow-hidden'>
+              {image ? (
+                <AvatarImage
+                  src={image}
+                  alt='profile'
+                  className='object-cover w-full h-full bg-black'
+                />
+              ) : (
+                <div
+                  className={`uppercase h-32 w-32 md:w-48 md:h-48 text-5xl border-[1px] flex items-center justify-center rounded-full  ${getColor(
+                    selectedcolor
+                  )}`}
+                >
+                  {firstName
+                    ? firstName.charAt(0)
+                    : userInfo.empid.charAt(0)}
+                </div>
+              )}
+            </Avatar>
+            {hovered && (
+              <div
+                className='absolute inset-0 flex items-center justify-center bg-black/50 ring-fuchsia-50 rounded-full'
+                onClick={image ? handleDeleteImage : handleFileInputClick}
+              >
+                {image ? (
+                  <FaTrash className='text-white text-3xl cursor-pointer' />
+                ) : (
+                  <FaPlus className='text-white text-3xl cursor-pointer' />
+                )}
+              </div>
+            )}
+            <input
+              type='file'
+              ref={fileInputRef}
+              className='hidden'
+              onChange={handleImageChange}
+              name='profile-image'
+              accept='.png, .jpg, .jpeg, .svg, .webp'
+            />
+          </div>
+          <div className='flex min-w-22 md:min-w-64 flex-col gap-5 text-white items-center justify-center'>
+            <div className='w-full'>
+              <input
+                placeholder='Employee Id'
+                type='text'
+                disabled
+                value={userInfo.empid || ''}
+                className='rounded-lg p-6 bg-[#2c2e3b] border-none'
+              />
+            </div>
+            <div className='w-full'>
+              <input
+                placeholder='First Name'
+                type='text'
+                onChange={(e) => setfirstName(e.target.value)}
+                value={firstName}
+                className='rounded-lg p-6 bg-[#2c2e3b] border-none'
+              />
+            </div>
+            <div className='w-full'>
+              <input
+                placeholder='Last Name'
+                type='text'
+                onChange={(e) => setlastName(e.target.value)}
+                value={lastName}
+                className='rounded-lg p-6 bg-[#2c2e3b] border-none'
+              />
+            </div>
+            <div className='w-full flex gap-5'>
+              {colors.map((color, index) => (
+                <div
+                  className={`${color} h-8 w-8 rounded-full cursor-pointer transition-all duration-300 ${
+                    selectedcolor === index ? 'outline outline-white/50' : ''
+                  }`}
+                  key={index}
+                  onClick={() => setselectedcolor(index)}
+                ></div>
+              ))}
+            </div>
+          </div>
+          <div className='w-full'>
+            <Button
+              className='h-16 w-full bg-purple-700 hover:bg-purple-900 transition-all duration-300'
+              onClick={saveChange}
+            >
+              Save Changes
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
