d2ff29ec25e956347d3af1e3d1fee3dd32da6dd8
faizal <71105723+sfaizal786@users.noreply.github.com>
2025-07-03 10:25:23 -0700

    Update Authmiddleware.js

--- server/middleware/Authmiddleware.js
+++ server/middleware/Authmiddleware.js
@@ -1,15 +1,15 @@
+import { request, response } from "express";
 import jwt from "jsonwebtoken";
 
-// Middleware to verify JWT token from cookies
-export const verifyToken = (req, res, next) => {
-  const token = req.cookies?.jwt;
 
-  if (!token) return res.status(401).send("You are not authenticated");
 
-  jwt.verify(token, process.env.JWT_KEY, (err, payload) => {
-    if (err) return res.status(403).send("Token is invalid");
+export const verifyToken =(request,response,next) =>{
+     const token = request.cookies.jwt;
+     if(!token) return response.status(401).send("You are not Authenicated");
 
-    req.userId = payload.userId;
+   jwt.verify(token, process.env.JWT_KEY, async(err,payload)=>{
+    if(err) return response.status(403).send("Token Invalid");
+    request.userId= payload.userId;
     next();
-  });
-};
+   })
+    }
