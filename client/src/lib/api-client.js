251a1b99560bdb0151ad6a7b428364787e6839f4
faizal <71105723+sfaizal786@users.noreply.github.com>
2025-07-07 10:43:23 -0700

    Delete client directory

--- client/src/lib/api-client.js
+++
@@ -1,8 +0,0 @@
-// src/lib/api-client.js
-import axios from "axios";
-import { HOST } from "@/utils/constant.js";
-
-export const apiClient = axios.create({
-  baseURL: HOST,
-  withCredentials: true, // ✅ Ensures cookies (JWT) are always sent
-});
